9d6941e0752e5b36f44af967523c9fd67556168e
Layson Batista <117116524+LaysonBatista@users.noreply.github.com>
2023-10-10 21:45:22 -0300

    Add files via upload

--- Sap_FabricaAutomotiva/Area_Fabrica.php
+++ Sap_FabricaAutomotiva/Area_Fabrica.php
@@ -2,21 +2,20 @@
 
 include('conexao_Fabrica.php');
 
-
-$query = $dbh->prepare('SELECT area, COUNT(*) AS total FROM alocacao GROUP BY area;');
+$query = $dbh->prepare('SELECT area FROM alocacao GROUP BY area;');
 
 $query->execute();
 
 $totalAreas = $query->fetchAll();
 
-//echo '<pre>';
-//print_r ($totalAreas);
-//echo '</pre>'
+echo '<pre>';
+print_r($totalAreas);
+echo '<pre>';
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,15 +30,23 @@ $totalAreas = $query->fetchAll();
     for($area = 1; $area < 12; $area++){
         //print_r($totalAreas[$area]['total']);
         echo '<a href="detalhes_Area.php?idArea='.$area.'">';
-            if(!empty($totalAreas[$area]['total'])){
+            $cont = 0;
+            foreach($totalAreas as $linha){
+                //print_r($linha);
+                if($linha['area'] == $area){
+                    $cont ++;
+                }
+            }
+            //echo $cont;
+            if($cont > 0){
                 echo '<div class="div'.$area.' azul">';
             }
             else{
                 echo '<div class="div'.$area.' branco">';
             }
-                echo '<p style="text-align: center;">'.$area.'</p>';
+            echo '<p style="text-align: center;">'.$area.'</p>';
             echo'</div>
-        </a>';
+            </a>';
         }
     ?>
 </body>
